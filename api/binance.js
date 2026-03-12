482cfcc5fbebd94a8467a7bb2bdc92e37785974a
Lawrence <1stempirelord@gmail.com>
2026-03-12 13:15:28 +0100

    `Added console.log statement to print input object`

--- api/binance.js
+++ api/binance.js
@@ -23,7 +23,7 @@ export default async function handler ( req, res )
             exchange_domain = "binance.us",
             ...params
         } = input;
-
+        console.log( input );
         if ( !method )
         {
             return res.status( 400 ).json( { error: "method parameter required" } );
